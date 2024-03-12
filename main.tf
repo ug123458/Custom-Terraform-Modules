e40ad386dc27a941d730175ef1cd1bc0d41c7b7e
Utkarsh Gupta <utkarsh.gupta@celebaltech.com>
2024-03-12 18:56:03 +0530

    fixed errors

--- main.tf
+++ main.tf
@@ -37,7 +37,7 @@ module "nsg" {
       access                     = "Allow"
       protocol                   = "Tcp"
       source_port_range          = "*"
-      destination_port_range     = "3389"
+      destination_port_range     = "22"
       source_address_prefix      = "*"
       destination_address_prefix = "*"
   }]
