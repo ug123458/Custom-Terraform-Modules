ed3d0efcfda51ddde6080988cdceb0334d5da868
Utkarsh Gupta <utkarsh.gupta@celebaltech.com>
2024-03-12 18:16:21 +0530

    testing vnet as a module with rg

--- main.tf
+++ main.tf
@@ -2,3 +2,10 @@ resource "azurerm_resource_group" "resource_group" {
   name     = var.name
   location = var.location
 }
+
+resource "azurerm_virtual_network" "virtual_network" {
+  name                = "${var.name}-vnet"
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+  address_space       = ["10.0.0.0/16"]
+}
