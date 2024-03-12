c39063a97f762efc4b2f6285e1ec242fb0140048
Utkarsh Gupta <utkarsh.gupta@celebaltech.com>
2024-03-12 18:47:30 +0530

    fixed errors

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,1 @@
-output "id" {
-  value = azurerm_resource_group.resource_group.id
 
-}
-
-output "location" {
-  value = azurerm_resource_group.resource_group.location
-}
-
-output "name" {
-  value = azurerm_resource_group.resource_group.name
-}
