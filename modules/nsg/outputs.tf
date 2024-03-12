2526a188ca6db5ab17b536969b3f54021a648943
Utkarsh Gupta <utkarsh.gupta@celebaltech.com>
2024-03-12 18:44:28 +0530

    fixed errors

---
+++ modules/nsg/outputs.tf
@@ -0,0 +1,9 @@
+output "name" {
+
+  value = azurerm_network_security_group.nsg.name
+}
+
+output "id" {
+
+  value = azurerm_network_security_group.nsg.id
+}
---
+++ modules/nsg/variables.tf
@@ -0,0 +1,27 @@
+variable "prefix" {
+  type = string
+}
+
+variable "location" {
+  type = string
+
+}
+
+variable "resource_group_name" {
+  type = string
+
+}
+
+variable "security_rules" {
+  type = list(object({
+    name                       = string
+    priority                   = number
+    direction                  = string
+    access                     = string
+    protocol                   = string
+    source_port_range          = string
+    destination_port_range     = string
+    source_address_prefix      = string
+    destination_address_prefix = string
+  }))
+}
