19089f7c5a36e21fa8d4bf069df0bce2d8e45848
Utkarsh Gupta <ug123458@gmail.com>
2024-03-21 16:33:06 +0530

    Fixed Errors

--- main.tf
+++ main.tf
@@ -1,45 +1,54 @@
 # Create a resource group
 module "rg" {
-  source = "./modules/resource-group"
-  prefix = var.resource_group_name
+  source   = "./modules/resource-group"
+  prefix   = var.resource_group_name
   location = var.location
 }
 
 # Create a vnet 
 module "vnet" {
-  source = "./modules/vnet"
-  name = var.vnet_name
-  resource_group_name = module.rg.resource_group_name
+  source                  = "./modules/vnet"
+  name                    = var.vnet_name
+  resource_group_name     = module.rg.resource_group_name
   resource_group_location = module.rg.resource_group_location
-  address_space = var.address_space
+  address_space           = var.address_space
 }
 
 # Create a subnet
 module "subnet" {
-  source = "./modules/subnet"
-  name = var.subnet_name
-  resource_group_name = module.rg.resource_group_name
+  source               = "./modules/subnet"
+  name                 = var.subnet_name
+  resource_group_name  = module.rg.resource_group_name
   virtual_network_name = module.vnet.virtual_network_name
-  address_prefixes = var.address_prefixes
+  address_prefixes     = var.address_prefixes
 }
 
 module "storage_acc" {
-  source = "./modules/storage-account"
-  name = var.storage_account_name
-  resource_group_name = module.rg.resource_group_name
-  location = module.rg.resource_group_location
-  account_tier = "Standard"
+  source                   = "./modules/storage-account"
+  name                     = var.storage_account_name
+  resource_group_name      = module.rg.resource_group_name
+  location                 = module.rg.resource_group_location
+  account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
 
 module "appservice_plan" {
-  source = "./modules/appservice-plan"
-  name = var.app_service_plan_name
-  location = module.rg.resource_group_location
+  source              = "./modules/appservice-plan"
+  name                = var.app_service_plan_name
+  location            = module.rg.resource_group_location
   resource_group_name = module.rg.resource_group_name
-  sku_tier = "Standard"
-  sku_size = "S1"
+}
+
+resource "azurerm_app_service_plan" "example" {
+  name                = "example-app-service-plan"
+  location            = module.rg.resource_group_location
+  resource_group_name = module.rg.resource_group_name
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
 }
 
 # Create a function app
@@ -47,26 +56,21 @@ resource "azurerm_linux_function_app" "example" {
   name                       = var.function_app_name
   location                   = module.rg.resource_group_location
   resource_group_name        = module.rg.resource_group_name
-  
-  service_plan_id        = module.appservice_plan.id
+  service_plan_id            = azurerm_app_service_plan.example.id
   storage_account_name       = module.storage_acc.name
   storage_account_access_key = module.storage_acc.access_key
 
-  https_only                 = true
+  https_only = true
 
-  app_settings = {
-    FUNCTIONS_WORKER_RUNTIME = "node"
-    WEBSITE_RUN_FROM_PACKAGE = "1"
-  }
-  
+
+
   site_config {
-    always_on        = true
-    ftps_state       = "Disabled"
-    http2_enabled    = true
-    use_32_bit_worker = false
   }
+}
 
-  virtual_network_subnet_id = module.subnet.subnet_id
+resource "azurerm_app_service_virtual_network_swift_connection" "example" {
+  app_service_id = azurerm_linux_function_app.example.id
+  subnet_id      = module.subnet.subnet_id
 }
 
 # Create a SQL Server
@@ -85,22 +89,22 @@ resource "azurerm_mssql_server" "example" {
 
 # Create a SQL Database
 resource "azurerm_mssql_database" "example" {
-  name                = var.sql_database_name
-  server_id         = azurerm_mssql_server.example.id
-  collation           = "SQL_Latin1_General_CP1_CI_AS"
-  create_mode         = "Default"
+  name        = var.sql_database_name
+  server_id   = azurerm_mssql_server.example.id
+  collation   = "SQL_Latin1_General_CP1_CI_AS"
+  create_mode = "Default"
   lifecycle {
     prevent_destroy = true
   }
 }
 
 module "private_endpoint" {
-  source = "./modules/private-endpoint"
+  source                = "./modules/private-endpoint"
   private_endpoint_name = var.private_endpoint_name
-  location = module.rg.resource_group_location
-  resource_group_name = module.rg.resource_group_name
-  subnet_id = module.subnet.subnet_id
-  sql_server_id = azurerm_mssql_server.example.id
-  sql_server_name = var.sql_server_name
+  location              = module.rg.resource_group_location
+  resource_group_name   = module.rg.resource_group_name
+  subnet_id             = module.subnet.subnet_id
+  sql_server_id         = azurerm_mssql_server.example.id
+  sql_server_name       = var.sql_server_name
 
 }
